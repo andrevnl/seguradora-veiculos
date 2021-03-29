f1939935ab4f2d02b3a1e8b064b81d65fbd2a0d2
andrevnl <andrevnl@hotmail.com>
2021-03-28 21:02:17 -0300

    Mudando configuracao para gerar um war em vez de um jar

--- src/main/java/br/com/andre/visionnaire/seguradoraveiculos/SeguradoraVeiculosApplication.java
+++ src/main/java/br/com/andre/visionnaire/seguradoraveiculos/SeguradoraVeiculosApplication.java
@@ -2,13 +2,20 @@ package br.com.andre.visionnaire.seguradoraveiculos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
-public class SeguradoraVeiculosApplication {
+public class SeguradoraVeiculosApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SeguradoraVeiculosApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SeguradoraVeiculosApplication.class, args);
